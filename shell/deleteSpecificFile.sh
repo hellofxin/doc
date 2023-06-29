17509241575fed2ac3fa50937f5dd45a458a1a93
fxin <hellofxin@qq.com>
2023-06-29 17:51:27 +0800

    update shell

--- shell/deleteSpecificFile.sh
+++ shell/deleteSpecificFile.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
+stringSeparate="-----------------------------------------------------------------------------"
+stringBegin="=============================================================================="
+stringMark="##########################################################################################################################################"
+echo "$stringBegin"
+echo "$stringSeparate"
 
-echo "=============================================================================="
+# 配置是否进入目标目录
+cfgCD=true
+echo "cfgCD: $cfgCD"
+cfgTargetFilePattern="*.[cs]"
+echo "cfgTargetFilePattern: ${cfgTargetFilePattern}"
+workingRoot=`pwd`
+echo "workingRoot: $workingRoot"
 
+echo "$stringSeparate"
+echo "pwd: `pwd`"
 sourcedir=$1
-cd $sourcedir
 echo "sourcedir: ${sourcedir}"
+if [ $cfgCD = true ]; then
+    echo "cd: $sourcedir"
+    cd $sourcedir
+    workingRoot=.
+    echo "workingRoot: $workingRoot"
+else
+    workingRoot=$sourcedir
+    echo "workingRoot: $workingRoot"
+fi
+echo "pwd: `pwd`"
 
-targetFilePattern="*.[cs]"
-echo "targetFilePattern: ${targetFilePattern}"
 
 # get the all dir and subdir
 list_alldir(){
@@ -22,25 +42,29 @@ list_alldir(){
     done
 }
 
-dirlist=$(list_alldir $sourcedir)
+dirlist=$(list_alldir $workingRoot)
+# echo "dirlist: $dirlist"
 echo " " > filenames.txt
 echo "" > deletedFile.txt
 # 打印第9个字段(文件名)： >awk '/^-.*/{print tmpname1$9}' tmpname1="$tmpname"
 #删除空行： >sed '/^ *$/d '
-for element in ${sourcedir} ${dirlist[*]}
+for element in ${workingRoot} ${dirlist[*]}
 do
+    # echo "$stringSeparate"
     # echo "element: $element"
+    # echo -e "ls $element:\n `ls $element`"
     ls -l $element | awk '/^-.*/{print tmpname1$9}' tmpname1="$tmpname" | sed '/^ *$/d ' >> filenames.txt
-    find $element -type f -name ${targetFilePattern} >> deletedFile.txt
-    find $element -type f -name ${targetFilePattern} -delete
 done
 
+find $workingRoot -type f -name ${cfgTargetFilePattern} >> deletedFile.txt
+find $workingRoot -type f -name ${cfgTargetFilePattern} -delete
 
+echo "$stringSeparate"
 totalfileCount=$(cat filenames.txt | wc -l)
 echo "totalfileCount: ${totalfileCount}"
 deletedFileCount=$(cat deletedFile.txt | wc -l)
 echo "deletedFileCount: ${deletedFileCount}"
-echo "-----------------------------------------------------------------------------"
+echo "$stringSeparate"
 # 任意字符若干，.c .h .s .a结尾，打印第一个字段
 # 任意字符若干，.cc 结尾，打印第一个字段
 # 任意字符若干，.cpp 结尾，打印第一个字段
@@ -59,6 +83,6 @@ do
 done
 
 echo ""
-echo "-----------------------------------------------------------------------------"
+echo "$stringSeparate"
 echo "deleted file:"
 cat ./deletedFile.txt
