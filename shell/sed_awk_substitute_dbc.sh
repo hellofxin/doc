eea81378c32457b9faa440f58e85cb000d874d78
fxin <hellofxin@qq.com>
2023-08-17 18:52:02 +0800

    dbc.sh ok

--- shell/sed_awk_substitute_dbc.sh
+++ shell/sed_awk_substitute_dbc.sh
@@ -29,4 +29,25 @@ source2="00_BDCU_BCAN_GenDBC20230726.dbc"
 # OK awk
 # awk '/^BO_/{gsub($3, "ch0"$3, $3); print;}' ${source0}
 # OK sed
-sed -rn '/^BO_/{s/ (\w*):/ ch0_\1:/g; s/$/\n=========\n/g; p;}' ${source0}
+# sed -rn '/^BO_.*BDCU/{s/ (\w*):/ ch0_tx_\1:/g; s/$/\n=========\n/g; p;}' ${source0}
+
+
+sed -rn '
+/^BO_/{ 							# 先找BO_开头的行A
+	/BDCU/!{ 						# 如果行A不含有BDCU字符串
+		:b; h; n;{ 					# 将数据覆盖存入hold空间，读取下一行A+1
+			:c;				
+			/^$/b; 					# A+1为空时，退出本次循环，否则继续
+			/^BO_.*BDCU/b; 			# A+1含有BO和BDCU时，退出本次循环，否则继续
+			/^BO_/bb; 				# A+1含有BO,不含BDCU,跳转到:b
+			/\bSG_.*BDCU/!{			# A+1不含有目标数据SG BDCU，则
+				 H; n; bc;			# 向hold空间追加当前A+1行数据，读取下一行，跳转到:c
+			}						# A+1行数据含有目标数据SG BDCU
+			H; g; 					# 则向hold空间追加当前A+1行数据，并从hold空间获取全部数据
+			s/ (\w*):/ =====_\1:/; 
+			s/$/\n=========\n/g; 
+			p;
+		}
+	}
+}
+' ${source0}
